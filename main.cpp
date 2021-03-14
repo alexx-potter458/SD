99cf224fdc260ad97e9d5d273f2da8fec4b17b1f
Andrei <danielss_stef@yahoo.com>
2021-03-14 12:21:32 +0200

    creare clasa

--- main.cpp
+++ main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <set>
 
 using namespace std;
 
+class Multime {
+    public:
+        set<int> multime;
+        set<int>::iterator it;
+    Multime(){
+
+    }
+
+    void insereaza(int element){
+        this->multime.insert(element);
+    }
+
+    void afiseaza(){
+        for (this->it = this->multime.begin(); this->it != this->multime.end(); ++ this->it){
+            cout << *this->it << " ";
+        }
+        cout << '\n';
+    }
+        
+};
+
+
 int main()
 {
-    cout << "esti prost";
-    cout << "hai dreq azi";
-    cout << "Gata ba, merge!";
+    Multime multime;
+    multime.insereaza(2);
+    multime.afiseaza();
+    
 }
