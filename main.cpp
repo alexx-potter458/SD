f5100d9d36d740a633d6b605cb2490e1cca18664
Andrei <danielss_stef@yahoo.com>
2021-03-14 13:39:40 +0200

    test1

--- main.cpp
+++ main.cpp
@@ -22,10 +22,10 @@ class Multime {
         
 };
 
-void insereaza(Multime &mySet, int element){
+/*void insereaza(Multime &mySet, int element){
         mySet.multime.insert(element);
     }
-
+*/
 ////////////////////////// incercare cu vector
 
  class MultimeVector {
@@ -40,20 +40,37 @@ void insereaza(Multime &mySet, int element){
 
  };
 
-void inserare2(MultimeVector &multimeaMea, int elemet) {
+void insereaza(MultimeVector &multimeaMea, int elemet) {
     int* temp;
-    
-
-    temp = new int[multimeaMea.lungime+ 1];
+    temp = new int[multimeaMea.lungime + 1];
 
     copy(multimeaMea.multime, multimeaMea.multime + multimeaMea.lungime, temp); // Suggested by comments from Nick and Bojan
     delete [] multimeaMea.multime;
     multimeaMea.multime = temp;
 
     multimeaMea.multime[multimeaMea.lungime] = elemet;
-    multimeaMea.lungime++;
-
+    multimeaMea.lungime ++;
 }
+
+
+
+void sterge(MultimeVector &multimeaMea, int element){
+    int* temp;
+
+    temp = new int[multimeaMea.lungime - 1];
+    for (size_t i = 0; i < multimeaMea.lungime - 1; i ++){
+        if(multimeaMea.multime[i] == element){
+            continue;
+        }else{
+            temp[i] = multimeaMea.multime[i];
+        }
+        cout << temp[i]<< "   ||   ";
+    }
+    delete [] multimeaMea.multime;
+    multimeaMea.multime = temp;
+    multimeaMea.lungime --;
+}
+
 
 
 /////////////////
@@ -61,18 +78,22 @@ void inserare2(MultimeVector &multimeaMea, int elemet) {
 
 int main()
 {
-    Multime multime;
-    insereaza(multime, 2);
+    //Multime multime;
+   // insereaza(multime, 2);
     cout<< "gata";
     //multime.afiseaza();
 
     cout << "\n\n-------------------------\n\n\n";
 
     MultimeVector v;
-    inserare2(v,3);
-    inserare2(v,55);
-
+    for (size_t i = 0; i < 55; i ++)
+    {
+        insereaza(v, i);
+    }
+    
+    sterge(v, 30);
     for(int i=0; i< v.lungime; i++)
     cout << v.multime[i]<< " ";
+
     
 }
